32c37c5e63a07ca0de7ffee2c481180064a8bc07
GuillerminaJiménez <guillermina.jr@minatitlan.tecnm.mx>
2025-07-01 21:27:18 -0600

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,55 +1,58 @@
 function Izquierda () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P5, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
     pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P13, 1)
 }
 function Reversa () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P5, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P13, 1)
 }
 function Avanza () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    pins.digitalWritePin(DigitalPin.P2, 1)
-    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P5, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P13, 0)
 }
 function Parar () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P5, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
     pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
 }
 function Derecha () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P5, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
     pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P13, 0)
 }
 radio.onReceivedString(function (receivedString) {
+    if (receivedString == "Avanza") {
+        basic.showArrow(ArrowNames.South)
+        Avanza()
+    }
+    if (receivedString == "Izquierda") {
+        basic.showArrow(ArrowNames.West)
+        Izquierda()
+    }
+    if (receivedString == "Derecha") {
+        basic.showArrow(ArrowNames.East)
+        Derecha()
+    }
+    if (receivedString == "Reversa") {
+        basic.showArrow(ArrowNames.North)
+        Reversa()
+    }
     if (receivedString == "Parar") {
         basic.showIcon(IconNames.SmallDiamond)
         Parar()
     }
-    if (receivedString == "Avanza") {
-        basic.showArrow(ArrowNames.South)
-    }
-    if (receivedString == "Izquierda") {
-        basic.showArrow(ArrowNames.East)
-        Izquierda()
-    }
-    if (receivedString == "Derecha") {
-        basic.showArrow(ArrowNames.West)
-        Derecha()
-    }
-    if (receivedString == "Reversa") {
-        basic.showArrow(ArrowNames.South)
-    }
-    Reversa()
 })
 radio.setTransmitPower(7)
-radio.setGroup(100)
+radio.setGroup(120)
+pins.analogWritePin(AnalogPin.P0, 300)
+pins.analogWritePin(AnalogPin.P2, 300)
 Parar()
-basic.showIcon(IconNames.SmallDiamond)
+basic.showIcon(IconNames.Happy)
